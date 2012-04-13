84023b1cbcaac2d04d0d5b93d1f9412b7b5d5750
Ordie Dordie <politicalarnold@gmail.com>
2012-04-13 13:57:28 -0700

    precom

--- config/application.rb
+++ config/application.rb
@@ -58,7 +58,6 @@ module SampleApp
 
     # Enable the asset pipeline
     config.assets.enabled = true
-    config.assets.precompile += %w( *.js *.css )
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
