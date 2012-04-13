3578efee0bbe83364a81a00390494a16024d6c88
Ordie Dordie <politicalarnold@gmail.com>
2012-04-13 13:51:35 -0700

    precom

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,9 @@
+#Run preprocessing
+config.assets.precompile += %w( bootstrap.css )
+
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 SampleApp::Application.initialize!
-config.assets.precompile += %w( bootstrap.css )
 
