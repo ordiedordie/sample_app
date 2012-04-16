69fd6324dde33570ecbddddaf8b1a7fb5423f1f5
Ordie Dordie <politicalarnold@gmail.com>
2012-04-16 01:28:38 -0700

    Added passwd and encrypted passwords to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,17 @@
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime        not null
-#  updated_at :datetime        not null
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime        not null
+#  updated_at         :datetime        not null
+#  encrypted_password :string(255)
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
+  attr_accessible :email, :name, :password, :password_confirmation
+  attr_accessor   :password
   
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
@@ -18,5 +20,7 @@ class User < ActiveRecord::Base
   validates :email, :presence => true,
                     :format => {:with =>email_regex},
                     :uniqueness => true
-  
+  validates :password, :presence => true,
+                       :confirmation => true,
+                       :length => {:within => 6..40}
 end
